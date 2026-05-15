c4c976d37c39f0a5b95cc97fc30afd0d92d0b81f
Aanya Jain <aanya31102007@gmail.com>
2026-05-15 16:14:47 +0530

    Changed api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const API_BASE =
-  process.env.REACT_APP_API_URL || "https://taxpro-backend-xi90.onrender.com";
+  process.env.REACT_APP_API_URL || "https://taxpro-backend-xi90.onrender.com/api";
 
 export default API_BASE;
