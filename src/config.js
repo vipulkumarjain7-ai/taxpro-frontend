4c333b05c2e34459634962c3bad4da9d13abce8d
Aanya Jain <aanya31102007@gmail.com>
2026-05-15 15:52:08 +0530

    Correct

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const API_BASE =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_URL || "https://taxpro-backend-xi90.onrender.com";
 
 export default API_BASE;
